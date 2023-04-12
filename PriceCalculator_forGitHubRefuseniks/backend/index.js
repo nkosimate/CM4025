4071d34a504d8bfb102a808a6a92a783165577d5
nkosimate <nkosimate@gmail.com>
2023-04-12 17:02:36 +0100

    fix button call

--- PriceCalculator_forGitHubRefuseniks/backend/index.js
+++ PriceCalculator_forGitHubRefuseniks/backend/index.js
@@ -12,33 +12,6 @@ var options = {
 
 var dir = path.join(__dirname, '../frontend');
 
-// Database stuff
-// Create a new MongoClient
-const client = new MongoClient(uri);
-async function run() {
-    try {
-        //Write databse Insert/Update/Query code here..
-        var dbo = client.db("mydb");
-        var myobj = { quoteName: n, salary: s, days: d }; //******CHECK!!!****
-        await dbo.collection("quotes").insertOne(myobj, function (err, res) {
-            if (err) {
-                console.log(err);
-                throw err;
-            }
-            console.log("1 quote inserted");
-        });
-        console.log('End the database stuff');
-
-    } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
-    }
-}
-run().catch(console.dir);
-
-
-
-
 app.get('/api', function (req, res) {
     res.send("Yes we have an API now")
 });
@@ -70,4 +43,48 @@ app.use(function (req, res, next) {
 
 app.listen(8000, function () {
     console.log('Listening on http://localhost:8000/');
+});
+
+app.get('/api/storeQuote', function(req, res){
+    //res.send("Hello world!")
+    // Copied from front end
+    var n = req.query.quoteName
+    var s = req.query.salary;
+    var d = req.query.days;
+    console.log("Storing quote: "+n+" "+s+" "+d)
+    console.log("Mongo URI is "+uri)
+
+    // Database stuff
+    // Create a new MongoClient
+    const client = new MongoClient(uri);
+    async function run() {
+    try {
+        // Connect the client to the server (optional starting in v4.7)
+        //await client.connect();
+        // Establish and verify connection
+        //await client.db("admin").command({ ping: 1 });
+        //console.log("Connected successfully to server");
+        console.log('Start the database stuff');
+        //Write databse Insert/Update/Query code here..
+        var dbo = client.db("mydb");
+        var myobj = { quoteName: n, salary: s, days: d };
+        await dbo.collection("quotes").insertOne(myobj, function(err, res) {
+            if (err) {
+                console.log(err); 
+                throw err;
+            }
+            console.log("1 quote inserted");
+        }); 
+        console.log('End the database stuff');
+
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+    }
+    run().catch(console.dir);
+
+
+
+    res.send("stored "+n)
 });
